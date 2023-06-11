f40fd9b085ae42102f4eac4ae3ef979b4f6c33f1
Dimriu <dimriudobrunin@gmail.com>
2023-06-11 22:40:36 +0300

    dependency setting

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,7 +1,5 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
@@ -9,27 +7,28 @@ public class CashbackHackServiceTest {
     public void shouldReturnIfAmount1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        Assert.assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void shouldReturnIfAmount900() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
-        int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        Assert.assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnIfAmount1100() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1100;
-        int actual = cashbackHackService.remain(amount);
         int expected = 900;
-        Assert.assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(expected, actual);
     }
 
 }
